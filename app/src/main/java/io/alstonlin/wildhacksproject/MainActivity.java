c58b30b7a4d0cad936f9198140f260816a980bb9
Alston Lin <alstonlin812@gmail.com>
2015-11-21 13:55:27 -0600

    Initial Commit

---
+++ app/src/main/java/io/alstonlin/wildhacksproject/MainActivity.java
@@ -0,0 +1,24 @@
+package io.alstonlin.wildhacksproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
