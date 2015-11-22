0bff070b1db49b488dc7325cc47952f252143f49
Alston Lin <alstonlin812@gmail.com>
2015-11-22 05:51:20 -0600

    Fixed the errors in the last merge

--- app/src/main/java/io/alstonlin/wildhacksproject/EventList.java
+++ app/src/main/java/io/alstonlin/wildhacksproject/EventList.java
@@ -59,22 +59,26 @@ public class EventList {
 
         @Override
         public View getView(final int i, View view, ViewGroup viewGroup) {
-            if(view==null) {
+            if (view == null) {
                 LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 view = vi.inflate(R.layout.events_list_item, viewGroup, false);
             }
             ((TextView)view.findViewById(R.id.name)).setText(evs.get(i).getName());
             ((TextView)view.findViewById(R.id.location)).setText(evs.get(i).location);
-            if(evs.get(i).getName().equals("Add New")){
+            if (evs.get(i).getName().equals("Add New")){
                 ((ImageView)view.findViewById(R.id.imageView3)).setImageDrawable(context.getDrawable(R.drawable.ic_my_library_add_indigo_a200_24dp));
             }
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(context, AppActivity.class);
+                    Intent intent;
+                    if(evs.get(i).getName().equals("Add New")){
+                        intent = new Intent(context, AddEventsActivity.class);
+                    }else {
+                        intent = new Intent(context, AppActivity.class);
+                    }
                     intent.putExtra(MainActivity.EXTRA_CODE, evs.get(i).getEventID());
-                    intent.putExtra(MainActivity.EXTRA_INTERNET, true);
-
+                    intent.putExtra(MainActivity.EXTRA_INTERNET, false);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     context.startActivity(intent);
